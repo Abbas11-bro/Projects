afbc3bf56a503a704098f1ecff203e51750e490c
Shaikh Abbas Ali <shaikhabbasali1411@gmail.com>
2025-01-24 20:17:18 +0530

    Add files via upload

---
+++ Area of rec.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    //int length = 3;
+    //int breadth = 5;
+    int length, breadth;
+    printf("Enter the length\n");
+    scanf("%d, &length");
+    
+    printf("Enter the breadth\n");
+    scanf("%d, &breadth");
+
+    printf("Area of rectangle is %d, length*breadth");
+    return 0;
+}
---
+++ Simple Inte.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    float p = 15.1;
+    int r = 8;
+    int t = 4;
+    printf("The value of simple interest is %f", (p*r*t)/100);
+    return 0;
+}
